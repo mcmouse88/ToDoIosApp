e03d8f0ee895d917715a1b0b58fe5d0b324eea9a
Sergei Razdobudko <sergey.razdobudko.work@gmail.com>
2026-05-22 12:09:07 +0400

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Razdobudko Sergei on 22.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
