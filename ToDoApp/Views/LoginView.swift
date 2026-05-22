b35cf33edb7d44365303b40a4089009cc477ebba
Sergei Razdobudko <sergey.razdobudko.work@gmail.com>
2026-05-22 17:20:02 +0400

    Create LoginView

--- ToDoApp/Views/LoginView.swift
+++ ToDoApp/Views/LoginView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var email = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                // Header
+                HeaderView()
+                
+                // Login Form
+                Form {
+                    TextField("Email Address", text: $email)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Button {
+                        // Attempt log in
+                    } label: {
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 12)
+                                .foregroundColor(Color.blue)
+                            
+                            Text("Log in")
+                                .foregroundColor(Color.white)
+                                .bold()
+                        }
+                    }
+                }
+                
+                // Create Accunt
+                VStack {
+                    Text("New around here?")
+                    NavigationLink("Create an account") {
+                        RegisterView()
+                    }
+                }
+                .padding(.bottom, 50)
+                
+                Spacer()
+            }
+        }
     }
 }
 
